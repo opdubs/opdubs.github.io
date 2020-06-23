c1467edafec47f6ffb830067d5bd028a9b70ffa6
opdubs <opdubs@gmail.com>
2020-06-22 21:09:37 -0700

    centre content on full page
    Also started editing JS for graph images

--- js/script.js
+++ js/script.js
@@ -26,3 +26,34 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+var image_tracker = 'graph1';
+
+function swap() {
+  // var graph1 = document.getElementsByClassName('graph1');
+  //     graph1 = graph1.src;
+  // var graph2 = document.getElementsByClassName('graph2');
+  //     graph2 = graph2.src;
+  // var graph3 = document.getElementsByClassName('graph3');
+  //     graph3 = graph3.src;
+  // var graph4 = document.getElementsByClassName('graph4');
+  //     graph4 = graph4.src;
+  var graph = document.getElementsByClassName('graphimg');
+  if(image_tracker=='graph1'){
+     graph.src= "../media/graph1.png";
+     image_tracker='graph1';
+     }
+     else if(image_tracker=='graph2'){
+     graph.src='../media/graph2.png';
+     image_tracker='graph2';
+     }
+     else if(image_tracker=='graph3'){
+     graph.src='../media/graph3';
+     image_tracker='graph3';
+     }
+     else if(image_tracker=='graph4'){
+     graph.src='../media/graph4';
+     image_tracker='graph4';
+     }
+
+}
