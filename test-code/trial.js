3437c4ac9013db136fd3e4a9de499f015c450929
opdubs <opdubs@gmail.com>
2020-06-23 21:47:32 -0700

    JS Edit, Image Optimization, Graph position
    new folder 'test-code' includes JS trials to change graph image without causing page reload. Issue was not resolved with two different JS options, nor with picture optimization.

---
+++ test-code/trial.js
@@ -0,0 +1,71 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// var image_tracker = 'graph1';
+//
+// function changeImg(imgName) {
+//   image = document.getElementsByClassName('imgDisp');
+//   image.src = imgName;
+// }
+function changeImg(imgName) {
+  var element = document.getElementsByClassName("active");
+  element.classList.toggle("active");
+  imgName.classList.toggle("active")
+}
+//
+//
+//
+var graphIndex = 1;
+showGraph(graphIndex);
+
+// Next/previous controls
+function plusGraph(n) {
+  showGraph(graphIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showGraph(graphIndex = n);
+}
+
+function showGraph(n) {
+  var i;
+  var graphs = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > graphs.length) {graphIndex = 1}
+  if (n < 1) {graphIndex = graphs.length}
+  for (i = 0; i < graphs.length; i++) {
+      graphs[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  graphs[graphIndex-1].style.display = "block";
+  dots[graphIndex-1].className += " active";
+}
